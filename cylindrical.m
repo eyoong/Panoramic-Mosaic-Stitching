8296766e25ac9bfb96d2cf94e4b82ab8d65edfec
eyoong <eyoong@Eugenes-MacBook-Air.local>
2015-03-10 01:09:42 -0500

    Correction: this is the fixed cylindrical

--- cylindrical.m
+++ cylindrical.m
@@ -2,18 +2,34 @@ function [ cylin ] = cylindrical( image, f )
 
 xc = size(image,2)/2; %x-coordinate is columns
 yc = size(image,1)/2; %y-coordinate is rows
-cylin = zeros(2*xc, 2*yc,3);
+cylin = zeros(2*xc, 2*yc,2);
+maxtheta = 0;
+maxh = 0;
 
 for i=1:(2*yc)
     for j=1:(2*xc)
         theta = atan((j - xc)/f);
         h = (i - yc)/sqrt((j - xc)^2 + f^2);
+        if theta > maxtheta
+            maxtheta = theta;
+        end
+        if h > maxh
+            maxh = h;
+        end
         cyrow = round(f*theta + xc);
         cycol = round(f*h + yc);
-        cylin(cycol,cyrow,:) = image(i,j,:);
+        cylin(i,j,1) = cycol;
+        cylin(i,j,2) = cyrow;
     end
 end
 
+crop = zeros(f*round(maxh)+yc, f*round(maxtheta)+xc, 3);
+for i=1:(2*yc)
+    for j=1:(2*xc)
+        crop(cylin(i,j,1),cylin(i,j,2),:) = image(i,j,:);
+    end
+end
+cylin = crop;
 cylin = uint8(cylin);
 
 end
